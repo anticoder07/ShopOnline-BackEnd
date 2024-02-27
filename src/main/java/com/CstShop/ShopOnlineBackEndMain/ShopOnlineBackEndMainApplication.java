10598fd4b6b0106c8ed3e620add69078e9096e13
Cao Bá Hướng <122510132+anticoder07@users.noreply.github.com>
2024-02-27 18:36:18 +0700

    The base backend version 1.0

---
+++ src/main/java/com/CstShop/ShopOnlineBackEndMain/ShopOnlineBackEndMainApplication.java
@@ -0,0 +1,13 @@
+package com.CstShop.ShopOnlineBackEndMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOnlineBackEndMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopOnlineBackEndMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CstShop/ShopOnlineBackEndMain/ShopOnlineBackEndMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CstShop.ShopOnlineBackEndMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineBackEndMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
